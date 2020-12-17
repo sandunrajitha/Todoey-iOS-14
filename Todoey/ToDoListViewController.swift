76011488d7ec05ee3a579678c2a00f39f1492ecb
Sandun Rajitha <rajitha1911@gmail.com>
2020-12-17 20:51:38 +0530

    finish add new item button and IBAction

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray = ["buy apple", "buy milk", "buy cheese", "buy natto"]
+    var itemArray = ["buy apple", "buy milk", "buy cheese", "buy natto"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,8 +22,49 @@ class ToDoListViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        print(indexPath)
         cell.textLabel!.text = "\(itemArray[indexPath.row])"
         return cell
     }
+    
+    // MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let cell = tableView.cellForRow(at: indexPath){
+            if cell.accessoryType == .checkmark{
+                cell.accessoryType = .none
+            } else {
+                cell.accessoryType = .checkmark
+            }
+            
+            cell.setSelected(false, animated: true)
+        }
+        
+    }
+    
+    // MARK: - Add new item
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (newItemTextField) in
+            newItemTextField.placeholder = "Enter new todo item"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Add Item", style: .default, handler: { (alertAction) in
+            
+            if let newitem = alert.textFields?[0].text{
+                if newitem != ""{
+                    self.itemArray.append(newitem)
+                    self.tableView.reloadData()
+                }
+            }
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
 }
