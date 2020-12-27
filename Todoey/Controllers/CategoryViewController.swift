99c629aae85a1344af7a0b2eab748494ff07a7ef
Sandun Rajitha <rajitha1911@gmail.com>
2020-12-27 16:52:46 +0530

    add category view controller and category entity

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,28 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Sandun Liyanage on 12/27/20.
+//
+
+import UIKit
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+    @IBAction func addCategoryPressed(_ sender: UIBarButtonItem) {
+    }
+    
+}
